97cb4b2b08ae96febca20f63222d1f2890d058ac
Bryan Choong <bryanchoong12@gmail.com>
2023-08-16 20:53:15 +0800

    done personalize for employer and jobseeker

---
+++ pages/personalize/employer/index.tsx
@@ -0,0 +1,7 @@
+import { PersonalizedMenu } from "../components/personalizedMenu";
+
+export default function EmployerPersonalize() {
+    return (
+        < PersonalizedMenu role="employer" />
+    )
+}
---
+++ pages/personalize/jobseeker/index.tsx
@@ -0,0 +1,7 @@
+import { PersonalizedMenu } from "../components/personalizedMenu";
+
+export default function JobSeekerPersonalize() {
+    return (
+        < PersonalizedMenu role="jobseeker" />
+    )
+}
