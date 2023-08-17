e39ca5e2d557981cb6f62ba94f272d636f9f0ccc
cheongyeechian <cheongyeechian@gmail.com>
2023-08-17 15:01:42 +0800

    feat: fix

--- pages/personalize/employer_viewpage/index.tsx
+++ pages/personalize/employer_viewpage/index.tsx
@@ -1,14 +1,9 @@
 import { PersonalizedMenu } from "../components/personalizedMenu";
-import Button  from "../../../components/Button";
 
-export default function EmployerViewPage() {
+export default function EmployerPersonalize() {
 
     return (
-        <>
+
         <PersonalizedMenu role="employer" />
-        <Button loading={false}>Sectors</Button>
-        <Button loading={false}>Skills</Button>
-        <Button loading={false}>Experiences</Button>
-        </>
     )
 }
