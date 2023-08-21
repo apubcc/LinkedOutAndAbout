3ba0ed28b3d612c52a3053d358e31a1ca1af15af
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-08-22 03:49:36 +0800

    feat: init sector resolver contract for post verification

--- contracts/src/SectorResolver.sol
+++ contracts/src/SectorResolver.sol
@@ -1,33 +1,37 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// pragma solidity 0.8.19;
+pragma solidity 0.8.19;
 
-// import { SchemaResolver } from "./SchemaResolver.sol";
+import {SchemaResolver} from "./SchemaResolver.sol";
 
-// import { IEAS, Attestation } from "./IEAS.sol";
+import {IEAS, Attestation} from "./interfaces/IEAS.sol";
 
-// import {nftPost} from  './nftPost.sol';
+import {Post} from "./Post.sol";
 
-// /**
-//  * @title A sample schema resolver that checks whether the attestation is from a specific attester.
-//  */
-// contract AttesterResolver is SchemaResolver {
+/**
+ * @title A sample schema resolver that checks whether the attestation is from a specific attester.
+ */
+contract AttesterResolver is SchemaResolver {
+    Post public postContract;
 
-//     nftPost public nftPostContract;
+    //connect the above contract to this contract
 
-//     //connect the above contract to this contract
+    constructor(IEAS eas, address postContractAddr) SchemaResolver(eas) {
+        postContract = Post(postContractAddr);
+    }
 
+    function onAttest(
+        Attestation calldata attestation,
+        uint256 /*value*/
+    ) internal view override returns (bool) {
+        //replace with a way to check that the source sector is the same as the target sector contained in the NFT contract
+        return true;
+    }
 
-//     constructor(IEAS eas, address nftPostContractAddr) SchemaResolver(eas) {
-//         nftPostContract = nftPost(nftPostContractAddr);
-//     }
-
-//     function onAttest(Attestation calldata attestation, uint256 /*value*/, string postSector, uint256 postId) internal view override returns (bool) {
-//         //replace with a way to check that the source sector is the same as the target sector contained in the NFT contract
-//         return nftPostContract.getTweet(postId)['postSector'] == postSector;
-//     }
-
-//     function onRevoke(Attestation calldata /*attestation*/, uint256 /*value*/) internal pure override returns (bool) {
-//         return true;
-//     }
-// }
+    function onRevoke(
+        Attestation calldata /*attestation*/,
+        uint256 /*value*/
+    ) internal pure override returns (bool) {
+        return true;
+    }
+}
