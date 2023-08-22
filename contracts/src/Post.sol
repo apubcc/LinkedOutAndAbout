5391ed00d1a4b02f043672330581368d6bae5306
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-08-23 06:03:18 +0800

    feat: improve post contract to save gas

--- contracts/src/Post.sol
+++ contracts/src/Post.sol
@@ -4,6 +4,18 @@ pragma solidity ^0.8.9;
 import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Post {
+    // Sector resolver contract address
+    address public sectorResolverAddress;
+
+    modifier onlySectorResolver() {
+        require(
+            msg.sender == sectorResolverAddress,
+            "Only Sector Resolver can call this function"
+        );
+        _;
+    }
+
+    // Events
     event PostCreated(
         uint256 postId,
         string postContent,
@@ -17,9 +29,8 @@ contract Post {
         address postAttester
     );
 
-    // strcture of the post
+    // Post structure
     struct PostStruct {
-        uint256 postId;
         string postContent;
         uint256 postAttestCount;
         uint256 postDate;
@@ -33,27 +44,31 @@ contract Post {
     mapping(uint256 => PostStruct) private idToPosts;
     mapping(address => uint256[]) private userToPostIds;
 
-    // create post
+    // Update sector resolver address
+    function updateSectorResolverAddress(
+        address _sectorResolverAddress
+    ) external {
+        sectorResolverAddress = _sectorResolverAddress;
+    }
+
+    // Create post
     function createPost(
         string calldata _postContent,
         uint256 _postDate,
         string calldata _postSector
-    ) public {
-        require(bytes(_postContent).length > 0, "No Tweet!");
+    ) external {
+        require(bytes(_postContent).length > 0, "No Post Content!");
         require(bytes(_postSector).length > 0, "No Sector!");
         require(_postDate > 0, "No Date!");
 
-        //increment by 1
         _postIds.increment();
-        // current id
         uint256 postId = _postIds.current();
         PostStruct storage post = idToPosts[postId];
-        post.postId = postId;
         post.postContent = _postContent;
         post.postDate = _postDate;
         post.postCreator = msg.sender;
         post.postSector = _postSector;
-        //add address to postIds
+
         userToPostIds[msg.sender].push(postId);
 
         emit PostCreated(
@@ -65,52 +80,63 @@ contract Post {
         );
     }
 
-    //get post
-    function getPost(uint256 postId) public view returns (PostStruct memory) {
+    // Get post
+    function getPost(uint256 postId) external view returns (PostStruct memory) {
         return idToPosts[postId];
     }
 
-    //get all posts
-    function getAllPosts() public view returns (PostStruct[] memory) {
-        PostStruct[] memory posts = new PostStruct[](_postIds.current());
-        for (uint256 i = 0; i < _postIds.current(); i++) {
-            posts[i] = idToPosts[i + 1];
+    // Get post sector
+    function getPostSector(
+        uint256 postId
+    ) external view returns (string memory) {
+        return idToPosts[postId].postSector;
+    }
+
+    // Get all posts with pagination
+    function getAllPosts(
+        uint256 start,
+        uint256 end
+    ) external view returns (PostStruct[] memory) {
+        uint256 lastPostId = _postIds.current();
+        require(
+            start <= end && end <= lastPostId,
+            "Invalid start or end values"
+        );
+
+        uint256 length = end - start + 1;
+        PostStruct[] memory posts = new PostStruct[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            posts[i] = idToPosts[start + i];
         }
+
         return posts;
     }
 
-    //get all posts by user
+    // Get all posts by user with pagination
     function getAllPostsByUser(
-        address user
-    ) public view returns (PostStruct[] memory) {
+        address user,
+        uint256 start,
+        uint256 end
+    ) external view returns (PostStruct[] memory) {
         uint256[] memory postIds = userToPostIds[user];
-        PostStruct[] memory posts = new PostStruct[](postIds.length);
-        for (uint256 i = 0; i < postIds.length; i++) {
-            posts[i] = idToPosts[postIds[i]];
+        require(
+            start <= end && end < postIds.length,
+            "Invalid start or end values"
+        );
+
+        uint256 length = end - start + 1;
+        PostStruct[] memory posts = new PostStruct[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            posts[i] = idToPosts[postIds[start + i]];
         }
+
         return posts;
     }
 
-    //get all posts by sector
-    function getAllPostsBySector(
-        string calldata sector
-    ) public view returns (PostStruct[] memory) {
-        PostStruct[] memory posts = new PostStruct[](_postIds.current());
-        uint256 postCount = 0;
-        for (uint256 i = 0; i < _postIds.current(); i++) {
-            if (
-                keccak256(bytes(idToPosts[i + 1].postSector)) ==
-                keccak256(bytes(sector))
-            ) {
-                posts[postCount] = idToPosts[i + 1];
-                postCount++;
-            }
-        }
-        return posts;
-    }
-
-    //function to update attestation count in post
-    function updateAttestCount(uint256 postId) public {
+    // Function to update attestation count in post
+    function updateAttestCount(uint256 postId) external onlySectorResolver {
         idToPosts[postId].postAttestCount++;
         emit PostAttested(
             postId,
