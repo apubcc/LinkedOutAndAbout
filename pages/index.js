188913504c3f7443bdf6428081eb6652718c6ab0
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-08-24 17:16:35 +0800

    feat: siwe done

--- pages/index.js
+++ pages/index.js
@@ -2,109 +2,27 @@
 import { Suspense, useEffect, useState } from "react";
 import { useAccount, useBalance } from "wagmi";
 import { Button, Layout, Loader, WalletOptionsModal } from "../components";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import React, { useRef } from 'react'
-import { useGLTF, useAnimations, PerspectiveCamera } from '@react-three/drei'
+import { PerspectiveCamera } from '@react-three/drei'
 import { OrbitControls } from '@react-three/drei'
 import { SSX } from '@spruceid/ssx';
+import EthLogo from '../components/EthLogo';
 
 const signInButtonHandler = async () => {
   const ssx = new SSX();
   const session = await ssx.signIn();
-  console.log(session);
+  // store session into localstorage
+  localStorage.setItem('session', JSON.stringify(session));
 };
 
-function EthLogo(props) {
-  const group = React.useRef();
-  const { nodes, materials, animations } = useGLTF('/scene.gltf');
-  const { actions } = useAnimations(animations, group);
-
-  //set for rotation
-  const [rotation, setRotation] = React.useState(0);
-
-  useFrame(() => {
-    setRotation(oldRotation => oldRotation + 0.01);
-  });
-
-  return React.createElement(
-    'group',
-    {
-      ref: group,
-      ...props,
-      dispose: null,
-      rotation: [0, rotation, 0]
-    },
-    React.createElement(
-      'group',
-      { name: "Sketchfab_Scene" },
-      React.createElement(
-        'group',
-        { name: "Sketchfab_model", rotation: [-Math.PI / 2, 0, 0], scale: 1.107 },
-        React.createElement(
-          'group',
-          { name: "161ee2e266e94c7791940d78a5a83d04fbx", rotation: [Math.PI / 2, 0, 0], scale: 0.01 },
-          React.createElement(
-            'group',
-            { name: "Object_2" },
-            React.createElement(
-              'group',
-              { name: "RootNode" },
-              React.createElement(
-                'group',
-                { name: "eth", rotation: [-Math.PI, -1.556, -Math.PI], scale: [1, 1, 1.145] },
-                React.createElement(
-                  'group',
-                  { name: "Pyramid_3", position: [0.217, 73.807, 0.929], rotation: [0, Math.PI / 4, 0] },
-                  React.createElement(
-                    'mesh',
-                    { name: "Pyramid_3_lambert3_0", geometry: nodes.Pyramid_3_lambert3_0.geometry, material: materials.lambert3 }
-                  ),
-                  React.createElement(
-                    'mesh',
-                    { name: "Pyramid_3_lambert4_0", geometry: nodes.Pyramid_3_lambert4_0.geometry, material: materials.lambert4 }
-                  ),
-                  React.createElement(
-                    'mesh',
-                    { name: "Pyramid_3_lambert2_0", geometry: nodes.Pyramid_3_lambert2_0.geometry, material: materials.lambert2 }
-                  )
-                ),
-                React.createElement(
-                  'group',
-                  { name: "Pyramid_1", position: [-0.108, -36.302, 32.916], rotation: [-Math.PI / 2, 0, -Math.PI] },
-                  React.createElement('group', { name: "transform1" })
-                ),
-                React.createElement(
-                  'group',
-                  { name: "Pyramid_2", position: [-0.108, -36.302, -33.845], rotation: [-Math.PI / 2, 0, 0] },
-                  React.createElement('group', { name: "transform2" })
-                )
-              ),
-              React.createElement(
-                'group',
-                { name: "Pyramid" },
-                React.createElement(
-                  'mesh',
-                  { name: "Pyramid_Default_Material_0", geometry: nodes.Pyramid_Default_Material_0.geometry, material: materials.Default_Material }
-                ),
-                React.createElement(
-                  'mesh',
-                  { name: "Pyramid_lambert4_0", geometry: nodes.Pyramid_lambert4_0.geometry, material: materials.lambert4 }
-                ),
-                React.createElement(
-                  'mesh',
-                  { name: "Pyramid_lambert3_0", geometry: nodes.Pyramid_lambert3_0.geometry, material: materials.lambert3 }
-                )
-              )
-            )
-          )
-        )
-      )
-    )
-  );
+const SignOutButtonHandler = async () => {
+  // remove session from localstorage
+  localStorage.removeItem('session');
 }
 
-
 const Home = () => {
+  const [userWallet, setUserWallet] = useState(null); 
   const [showWalletOptions, setShowWalletOptions] = useState(false);
   const [{ data: accountData, loading: accountLoading }] = useAccount();
   const [{ data: balanceData, loading: balanceLoading }] = useBalance({
@@ -112,22 +30,72 @@ const Home = () => {
     watch: true,
   });
 
+  async function signInOnClick() {
+    await signInButtonHandler();
+    
+    var session2 = localStorage.getItem('session');
+    if (session2) {
+      setUserWallet(session2);
+    }
+  }
+
+  function getWalletAddress(stringJson) {
+    var obj = JSON.parse(stringJson);
+    return obj.address;
+  }
+
+  function signOutOnClick() {
+    SignOutButtonHandler();
+    setUserWallet(null);
+  }
+
+  useEffect(() => {
+    var session2 = localStorage.getItem('session');
+    if (session2) {
+      setUserWallet(session2);
+    }
+    // read localstorage session and set userWallet
+  }, [userWallet]);
+
   const loading = (accountLoading || balanceLoading) && !balanceData;
 
   const renderContent = () => {
     if (loading) return <Loader size={8} />;
-    // if (balanceData) {
-    //   return (
-    //     <>
-    //       <h1 className="mb-8 text-4xl font-bold">My Wallet</h1>
-    //       <div className="inline-flex place-items-center">
-    //         <h6 className="ml-2 text-2xl">{`Ξ ${Number(
-    //           balanceData?.formatted
-    //         ).toFixed(4)} ${balanceData?.symbol}`}</h6>
-    //       </div>
-    //     </>
-    //   );
-    // }
+    if (balanceData) {
+      return (
+        <>
+          <div className="w-[350px] h-[350px]">
+            <Canvas>
+              <PerspectiveCamera position={[0, 0, 5]} makeDefault={true} />
+              <Suspense fallback={null}>
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} />
+                <EthLogo position={[0,-1,0]} scale={[1.25,1.25,1.25]}/>
+                <OrbitControls target={[0,0,0]} enableZoom={false}/>
+              </Suspense>
+            </Canvas>
+          </div>
+  
+          <h1 className="mb-8 text-4xl font-bold">
+            Wallet Connected! Sign-in With Ethereum to Verify Your Identity
+          </h1>
+
+          { userWallet ? 
+          <div className="flex items-center justify-between">
+            <h2 className="mr-8 text-2xl font-semi-bold">Signed in as { getWalletAddress(userWallet) }</h2>
+            <Button loading={accountLoading} onClick={() => signOutOnClick()}>
+              Sign-out
+            </Button>
+            </div>
+            :
+            <Button loading={accountLoading} onClick={() => signInOnClick()}>
+              Sign-in With Ethereum
+            </Button>
+          }
+        </>
+      );
+    }
 
     return (
       <>
@@ -147,9 +115,9 @@ const Home = () => {
         <h1 className="mb-8 text-4xl font-bold">
           Welcome to LinkedOutAndAbout!
         </h1>
-        <Button loading={accountLoading} onClick={() => signInButtonHandler(true)}>
-          Sign-in With Ethereum
-        </Button>
+        <h2 className="mb-8 text-2xl font-semi-bold">
+          Connect Your Wallet to Get Started
+        </h2>
       </>
     );
   };
