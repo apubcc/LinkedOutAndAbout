bcb2a9990a5ec2df23d19a56ac7a703b9a62c89a
B3njam1n3012 <tankerkzheng1@gmail.com>
2023-08-27 04:52:55 +0800

    feat: profile done migrate

--- pages/profile/employer/index.js
+++ pages/profile/employer/index.js
@@ -1,70 +1,72 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import Image from "next/image";
 //import type { NextPage } from "next";
-import { useAccount, useBalance, useEnsAvatar, useEnsResolver, useEnsName } from "wagmi";
-import { Button, Layout, Loader, WalletOptionsModal } from "../../../components";
+import { useAccount, useEnsAvatar, useEnsName } from "wagmi";
+import { Button, Layout, Loader } from "../../../components";
 import  FrostedCard from "../../../components/FrostedCard";
 import ProfileFrame from "../../../components/ProfileFrame";
-import React from 'react'
-
-function ensName() {
-    //ts-ignore
-      const { data, isError, isLoading } = ({
-        name: 'awkweb.eth',
-      })
-     
-      if (isLoading) return <div>Fetching resolver…</div>
-      if (isError) return <div>Error fetching resolver</div>
-      return <div>Resolver: {JSON.stringify(data)}</div>
-    }
 
 const Home = () => {
-  const [showWalletOptions, setShowWalletOptions] = useState(false);
-  const [{ data: accountData, loading: accountLoading }] = useAccount();
-  const [{ data: balanceData, loading: balanceLoading }] = useBalance({
-    addressOrName: accountData?.address,
-    watch: true,
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
+  const { data: ensName, isError: ensNameError, isLoading: ensNameLoading } = useEnsName({
+    address: address,
   });
-
-  const { data: ensResolverData, loading: ensLoading } = useEnsResolver(accountData?.address);
-
-  const displayAddressOrEns = () => {
-    if (ensLoading) return "Resolving ENS...";
-    // Assuming ensResolverData contains the ENS name; adjust if the structure is different
-    const userId = ensResolverData?.ensName || accountData?.address || "Address not found";
-    return <><strong>User ID:</strong> {userId}</>;  // Using JSX to bold the "User ID:" string
-  };
-
-  const loading = (accountLoading || balanceLoading) && !balanceData;
+  const { data: ensAvatar, isError: ensAvatarError, isLoading: ensAvatarloading } = useEnsAvatar({
+    name: ensName,
+  })
 
   const renderContent = () => {
-    if (loading) return <Loader size={8} />;
-
-    if (balanceData) {
+    if (isConnecting) return <Loader size={8} />;
+    if (isConnected) {
       return (
         <>
           <h1 className = "mb-0.8 text-4xl font-semibold text-gray-800">Profile Info:</h1> {/* Typography enhancement */}
           <div className = "grid place-items-center">
             <FrostedCard className = "shadow-lg bg-white bg-opacity-50 w-auto h-auto p-6 space-y-4"> {/* Added shadow and subtle background */}
-              <div className = "flex items-center space-x-6 mb-4">
-                {/* Pass ensResolverData.ensName and ensResolverData.ensAvatar to ProfileFrame */}
-                <ProfileFrame
-                  address={ensResolverData?.ensName || accountData?.address}
-                  ensAvatar={ensResolverData?.ensAvatar} 
-                  size={102} // Jazzicon's Size
+              <div className = "grid place-items-center space-x-6 mb-4">
+              {ensAvatar ? 
+                <Image
+                  src={ensAvatar}
+                  alt="ENS Avatar"
+                  width={100}
+                  height={100}
+                  objectFit="cover"
+                  className="rounded-full"
                 />
-                <div className="text-lg text-gray-700">{displayAddressOrEns()}</div>
+                :
+                <Image
+                  src="/images/placeholder.png"
+                  alt="ENS Avatar"
+                  width={100}
+                  height={100}
+                  objectFit="cover"
+                  className="rounded-full"
+                />
+                }
+                <div className="text-lg text-gray-700">
+                  <div style={{ marginTop: '0.5rem' }}></div>
+                  <p style={{ fontSize: '0.8rem' }}>
+                    <strong>Level:</strong> attestationFormat
+                  </p>
+                </div>
               </div>
-              <p><strong>Sector:</strong> Tech</p>
-              <p><strong>Skills:</strong> SWE</p>
-              <p><strong>Experiences:</strong> Mobile Lead of OpenSea</p>
-              <p><strong>Level:</strong> attestationFormat</p>
+              <div style={{ marginBottom: '2.5rem' }}></div>
+              {ensName ?
+                <h1 className="text-2xl font-semibold text-gray-800">{ensName}</h1>
+                :
+                <h1 className="text-2xl font-semibold text-gray-800">{address}</h1>
+              }
+              <p><strong>Details: </strong>details(tags suggestion)</p>
+              <p><strong>Sector: </strong> Tech</p>
+              <p><strong>Skills: </strong>skills(Community)</p>
+              <p><strong>Languages: </strong>lang(HTML, CSS, Solidity)</p>
             </FrostedCard>
             <Button
               loading={false}
               onClick={() => attest("metIRL", "0x66263b35bae43592b4A46F4Fca4D8613987610d4")}
             >
               Attest
-            </Button>          
+            </Button>
       </div>
     </>
   );
@@ -73,29 +75,15 @@ const Home = () => {
     return (
       <>
         <h1 className="mb-8 text-4xl font-bold">
-          Employer Profile
+          Job Seeker Profile
         </h1>
-        <Button
-          loading={accountLoading}
-          onClick={() => setShowWalletOptions(true)}
-        >
-          Connect to Wallet
-        </Button>
       </>
     );
   };
   
   return (
     <>
-      <WalletOptionsModal
-        open={showWalletOptions}
-        setOpen={setShowWalletOptions}
-      />
-
-      <Layout
-        showWalletOptions={showWalletOptions}
-        setShowWalletOptions={setShowWalletOptions}
-      >
+      <Layout>
         <div className="grid h-screen place-items-center">
           <div className="grid place-items-center">{renderContent()}</div>
         </div>
--- pages/profile/jobseeker/index.js
+++ pages/profile/jobseeker/index.js
@@ -1,59 +1,62 @@
 import React, { useState } from 'react';
+import Image from "next/image";
 //import type { NextPage } from "next";
-import { useAccount, useBalance, useEnsAvatar, useEnsResolver, useEnsLookup } from "wagmi";
-import { Button, Layout, Loader, WalletOptionsModal } from "../../../components";
+import { useAccount, useEnsAvatar, useEnsName } from "wagmi";
+import { Button, Layout, Loader } from "../../../components";
 import  FrostedCard from "../../../components/FrostedCard";
 import ProfileFrame from "../../../components/ProfileFrame";
 
 const Home = () => {
-  const [showWalletOptions, setShowWalletOptions] = useState(false);
-  const [{ data: accountData, loading: accountLoading }] = useAccount();
-  
-  const [{ data: balanceData, loading: balanceLoading }] = useBalance({
-    addressOrName: accountData?.address,
-    watch: true,
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
+  const { data: ensName, isError: ensNameError, isLoading: ensNameLoading } = useEnsName({
+    address: address,
   });
-  
-  const [{ data, error, loading1 }] = useEnsLookup({
-    address: accountData?.address
-  });
-
-  const renderEnsName = () => {
-    if (loading1) return <div>Fetching name…</div>
-    if (error || !data) return <div>Error fetching name</div>
-    return <div>{data}</div>
-  };
-
-  const loading = (accountLoading || balanceLoading) && !balanceData;
+  const { data: ensAvatar, isError: ensAvatarError, isLoading: ensAvatarloading } = useEnsAvatar({
+    name: ensName,
+  })
 
   const renderContent = () => {
-    if (loading) return <Loader size={8} />;
-
-
-    if (balanceData) {
+    if (isConnecting) return <Loader size={8} />;
+    if (isConnected) {
       return (
         <>
           <h1 className = "mb-0.8 text-4xl font-semibold text-gray-800">Profile Info:</h1> {/* Typography enhancement */}
           <div className = "grid place-items-center">
             <FrostedCard className = "shadow-lg bg-white bg-opacity-50 w-auto h-auto p-6 space-y-4"> {/* Added shadow and subtle background */}
-              <div className = "flex items-center space-x-6 mb-4">
-                {/* Pass ensResolverData.ensName and ensResolverData.ensAvatar to ProfileFrame */}
-                <ProfileFrame
-                  // address={ensResolverData?.ensName || accountData?.address}
-                  // ensAvatar={ensResolverData?.ensAvatar} 
-                  size={102} // Set the size for Jazzicon
+              <div className = "grid place-items-center space-x-6 mb-4">
+              {ensAvatar ? 
+                <Image
+                  src={ensAvatar}
+                  alt="ENS Avatar"
+                  width={100}
+                  height={100}
+                  objectFit="cover"
+                  className="rounded-full"
                 />
+                :
+                <Image
+                  src="/images/placeholder.png"
+                  alt="ENS Avatar"
+                  width={100}
+                  height={100}
+                  objectFit="cover"
+                  className="rounded-full"
+                />
+                }
                 <div className="text-lg text-gray-700">
                   <div style={{ marginTop: '0.5rem' }}></div>
-                  {/* call displayAddressOrEns */}
-                  {renderEnsName()}
                   <p style={{ fontSize: '0.8rem' }}>
                     <strong>Level:</strong> attestationFormat
                   </p>
                 </div>
               </div>
               <div style={{ marginBottom: '2.5rem' }}></div>
-             <p><strong>Details: </strong>details(tags suggestion)</p>
+              {ensName ?
+                <h1 className="text-2xl font-semibold text-gray-800">{ensName}</h1>
+                :
+                <h1 className="text-2xl font-semibold text-gray-800">{address}</h1>
+              }
+              <p><strong>Details: </strong>details(tags suggestion)</p>
               <p><strong>Sector: </strong> Tech</p>
               <p><strong>Skills: </strong>skills(Community)</p>
               <p><strong>Languages: </strong>lang(HTML, CSS, Solidity)</p>
@@ -70,32 +73,17 @@ const Home = () => {
 }
 
     return (
-      
       <>
         <h1 className="mb-8 text-4xl font-bold">
           Job Seeker Profile
         </h1>
-        <Button
-          loading={accountLoading}
-          onClick={() => setShowWalletOptions(true)}
-        >
-          Connect to Wallet
-        </Button>
       </>
     );
   };
   
   return (
     <>
-      <WalletOptionsModal
-        open={showWalletOptions}
-        setOpen={setShowWalletOptions}
-      />
-
-      <Layout
-        showWalletOptions={showWalletOptions}
-        setShowWalletOptions={setShowWalletOptions}
-      >
+      <Layout>
         <div className="grid h-screen place-items-center">
           <div className="grid place-items-center">{renderContent()}</div>
         </div>
