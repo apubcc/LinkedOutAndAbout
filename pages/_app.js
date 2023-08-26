bcc4ecc51e9d5f1bfc473e9e32e01477ec1a8c93
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-08-27 04:04:46 +0800

    feat: refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,27 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
-import { WagmiConfig, createConfig, configureChains } from 'wagmi'
-import {mainnet, arbitrumGoerli, avalancheFuji } from 'wagmi/chains'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
-import { infuraProvider } from 'wagmi/providers/infura'
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import { mainnet, arbitrumGoerli, avalancheFuji } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { infuraProvider } from "wagmi/providers/infura";
 import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
 // Configure chains & providers with the Alchemy & infura provider.
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, arbitrumGoerli, avalancheFuji],
-  [alchemyProvider({ apiKey: 'WV-NUphenL-PYZXxYFeoLOz73EdWjPVU' }), 
-    infuraProvider({ projectId: '688aab67fe3b42ed81a381edba65956e' }),
-  publicProvider()],
+  [
+    alchemyProvider({ apiKey: "WV-NUphenL-PYZXxYFeoLOz73EdWjPVU" }),
+    infuraProvider({ projectId: "688aab67fe3b42ed81a381edba65956e" }),
+    publicProvider(),
+  ]
 );
 
-const {connectors} = getDefaultWallets({
-  appName: 'LinkedOut',
-  projectId: 'f8001bbde7438d70814ffd4373f81cd6',
+const { connectors } = getDefaultWallets({
+  appName: "LinkedOut",
+  projectId: "f8001bbde7438d70814ffd4373f81cd6",
   chains,
 });
 
@@ -35,6 +39,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
-  )
+  );
 }
-
